4b0853228f355d1260b209ec529f4e534c860457
surajrk <surajmrk28@gmail.com>
2019-07-29 19:59:34 +0530

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angpro1';
+  addCheckbox: FormGroup;
+  createCheckBox: FormGroup;
+  a;
+  s = [
+  ];
+  style1;
+  
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+
+    this.addCheckbox = this.fb.group({
+      AddText: ['', [Validators.required]],
+    });
+    this.createCheckBox = this.fb.group({
+      check: null,
+    });
+  }
+  get fc() {
+    return this.addCheckbox.controls;
+  }
+  storeValue() {
+    // this.a = this.addCheckbox.value;
+    // console.log(this.a);
+    // this.createCheckBox.setValue(this.a);
+    console.log(this.fc.AddText.value);
+    this.s.push({ checked: false , text: (this.fc.AddText.value) });
+  }
+  deletetext(i) {
+
+    this.s.splice(i, 1);
+    console.log("deleted",i);
+  }
+  toggle(value) {
+    this.style1 = value;
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
